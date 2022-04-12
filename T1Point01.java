a7a61c8ceafd47291155c4fc112f5dcb6d227d08
AudryPutri <101167367+AudryPutri@users.noreply.github.com>
2022-04-12 10:20:29 +0800

    Setor Tugas 1

---
+++ T1Point01.java
@@ -0,0 +1,22 @@
+
+package t1point01;
+
+
+public class T1Point01 {
+
+   
+    public static void main(String[] args) {
+       String [][]data =
+       {
+       {"127647","129883","120495","123489"},
+{"Teknik Informatika","Sistem Komputer","Sistem Komputer","Teknik Informatika"}
+     
+       };    
+       System.out.println("Adi         "+(data[0][0])+"           "+data[1][0]);
+       System.out.println("Mika        "+(data[0][1])+"           "+data[1][1]);
+       System.out.println("Budi        "+(data[0][2])+"           "+data[1][2]);
+       System.out.println("Bunga       "+(data[0][3])+"           "+data[1][3]);
+       
+    }
+    
+}
---
+++ T1Point02.java
@@ -0,0 +1,23 @@
+
+package t1point02;
+
+public class T1Point02 {
+
+    public static void main(String[] args)
+    {
+        String [] Mahasiswa = 
+        {"Rara","Ayu","Mega","Tasya","susi"};
+        
+        System.out.println(Mahasiswa[0]);
+        System.out.println(Mahasiswa[1]);
+        System.out.println(Mahasiswa[2]);
+        System.out.println(Mahasiswa[3]);
+        System.out.println(Mahasiswa[4]);
+        
+        
+        
+        
+        
+    }
+    
+}
